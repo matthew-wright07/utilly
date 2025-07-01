03a7beb3b7f47926e78449eb15eb3cf82be8fc30
Matthew <matthewjwright07@gmail.com>
2025-06-30 22:26:33 -0500

    start componet update

--- components/Home/Start.js
+++ components/Home/Start.js
@@ -6,7 +6,7 @@ export default function Start(){
     return (
         <Wrapper>
             <div className="flex flex-col items-center justify-center border border-secondary py-8 gap-4 rounded-lg">
-                <h2>Lets Get Started</h2>
+                <h2>Let's Begin</h2>
                 <div className="flex gap-2">
                     <OutlineButton text="Explore" href="/about"/>
                     <Button text="Start" href="/tools"/>
