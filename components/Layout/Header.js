06d8e1b6c70ac5d4a690e19221ac443f7a3f552e
Matthew <matthewjwright07@gmail.com>
2026-01-01 01:02:05 -0600

    updates

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -2,13 +2,14 @@ import Button from "../Items/Button";
 import Wrapper from "../Wrappers/Wrapper";
 import OutlineButton from "../Items/OutlineButton";
 import HoverLink from "./HoverLink";
+import Image from "next/image";
 
 export default function Header(){
     return (
         <Wrapper>
             <header className="flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0">
                 <a href="/" className="flex gap-2 items-center">
-                    <img src="/logo.svg" className="w-8"/>
+                    <Image width={500} height={500} priority src="/logo.svg" className="w-8"/>
                     <h2>Utilly</h2>
                 </a>
                 <div className="flex gap-4">
